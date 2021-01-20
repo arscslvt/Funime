d8a8ee27ebd8a4132a26459dcc824252842a42c9
arscslvt <57394934+arscslvt@users.noreply.github.com>
2021-01-20 14:44:11 +0100

    Add files via upload

---
+++ Funime/index.php
@@ -0,0 +1,1 @@
+<?php header( 'Location: /funime/index.html' ) ;  ?>
--- Funime/script.js
+++ Funime/script.js
@@ -1,13 +1,56 @@
 document.getElementById("alert").style.display = "grid";
 document.getElementById("alertTitle").innerHTML = "Funime is in dev-mode!";
-document.getElementById("alertBody").innerHTML = "*factory sounds*, keep watching Funime. We're building it piece by piece. [283]";
+document.getElementById("alertBody").innerHTML = "[PREVIEW] *factory sounds*, keep watching Funime. We're building it piece by piece.";
+const errorName = document.getElementById("errorName");
+const errorBody = document.getElementById("errorBody");
+const fcode = document.getElementById("fcode");
 
 document.getElementById("aboutPage").addEventListener("click", function(){
     document.location.assign("about.html");
 })
 document.getElementById("learnmorePage").addEventListener("click", function(){
-    document.location.assign("learnmore.html");
+    error("morepage");
+})
+document.getElementById("createCode").addEventListener("click", function(){
+    error("morepage");
 })
 document.getElementById("conditionsPage").addEventListener("click", function(){
     document.location.assign("conditions.html");
 })
+
+fcode.addEventListener("keyup", function(event){
+    if (event.keyCode === 13) {
+        var code = fcode.value;
+        fcode.blur();
+        console.log(code);
+        code = code.toUpperCase();
+        console.log("up: " + code);
+        error(code);
+    }
+})
+
+function error(code){
+    switch(code){
+        case "JS73HD":
+            document.location.assign("room.html");
+        break;
+
+        case "morepage":
+            document.getElementById("error").style.display = "grid";
+            errorName.innerHTML = "Unavailable page.";
+            errorBody.innerHTML = "This feature is in dev-mode.<br>Follow us on Reddit for progress information.";
+            fcode.value = "";
+            fcode.blur();
+            setTimeout(function(){document.getElementById("error").style.display = "none";}, 7000);
+        break;
+
+        default: 
+            document.getElementById("error").style.display = "grid";
+            errorName.innerHTML = "Wait, wait, wait!";
+            errorBody.innerHTML = "Oops, you cannot access Funime. We are still assembling the pieces.<br>Follow us on Reddit for progress information.";
+            fcode.value = "";
+            fcode.blur();
+            setTimeout(function(){document.getElementById("error").style.display = "none";}, 7000);
+        break;
+    }
+}
